ccaf0000f832fa39a7801572ade5f0ff033ef961
tumakazijobtech-art <tumakazijobtech@gmail.com>
2026-09-08 09:16:38 +0300

    updated 2

--- artifacts/api-server/src/routes/index.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import healthRouter from "./health.js";
-import lownieRouter from "./lownie.js";
-const router = Router();
-router.use(healthRouter);
-router.use(lownieRouter);
-export default router;
